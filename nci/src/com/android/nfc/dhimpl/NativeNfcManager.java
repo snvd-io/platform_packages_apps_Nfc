8a62247dd11f65915460c485318456fc29fd91eb
John Johnson <johnrjohn@google.com>
2024-03-04 17:00:54 -0500

    Adding tracing for polling loop frames
    Test: https://ui.perfetto.dev/#!/?s=340ab21c032d0a7a78d4db535d87c98db6e918d0ffc279df04c359864f6bb300
    
    Change-Id: I1cec18982a78d927ac4c095d4597cd0aef39446b

--- nci/src/com/android/nfc/dhimpl/NativeNfcManager.java
+++ nci/src/com/android/nfc/dhimpl/NativeNfcManager.java
@@ -21,6 +21,7 @@ import android.nfc.cardemulation.PollingFrame;
 import android.nfc.tech.Ndef;
 import android.nfc.tech.TagTechnology;
 import android.os.Bundle;
+import android.os.Trace;
 import android.util.Log;
 
 import com.android.nfc.DeviceHost;
@@ -164,7 +165,7 @@ public class NativeNfcManager implements DeviceHost {
         }
 
         return mContext.getResources().getBoolean(
-            com.android.nfc.R.bool.nfc_observe_mode_supported);
+                com.android.nfc.R.bool.nfc_observe_mode_supported);
     }
 
     @Override
@@ -419,6 +420,7 @@ public class NativeNfcManager implements DeviceHost {
         if (data_len < MIN_POLLING_FRAME_TLV_SIZE) {
             return;
         }
+        Trace.beginSection("notifyPollingLoopFrame");
         Bundle frame = new Bundle();
         final int header_len = 4;
         int pos = header_len;
@@ -433,12 +435,14 @@ public class NativeNfcManager implements DeviceHost {
             int length = p_data[pos + TLV_len_offset];
             if (TLV_len_offset + length < TLV_gain_offset ) {
                 Log.e(TAG, "Length (" + length + ") is less than a polling frame, dropping.");
+                Trace.endSection();
                 return;
             }
             if (pos + TLV_header_len + length > data_len) {
                 // Frame is bigger than buffer.
                 Log.e(TAG, "Polling frame data ("+ pos + ", " + length
                         + ") is longer than buffer data length (" + data_len + ").");
+                Trace.endSection();
                 return;
             }
             switch (type) {
@@ -493,6 +497,7 @@ public class NativeNfcManager implements DeviceHost {
             pos += (TLV_header_len + length);
         }
         mListener.onPollingLoopDetected(frame);
+        Trace.endSection();
     }
 
     private void notifyWlcStopped(int wpt_end_condition) {
