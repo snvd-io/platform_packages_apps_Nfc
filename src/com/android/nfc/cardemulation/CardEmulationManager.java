snvd-io/platform_packages_apps_Nfc

commit 3d7ca5072cde5e61eb09b42702544bcc7f4c224a

Cleanup handling of payment service and WALLET role
Bug: 328466308
Test: manual
Change-Id: I4f07051448742847df619ace5eee20e4b0b7baea

src/com/android/nfc/cardemulation/CardEmulationManager.java
@@ -145,8 +145,6 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
         mOffHostRouteUicc = mRoutingOptionManager.getOffHostRouteUicc();
         mForegroundUid = Process.INVALID_UID;
         int currentUser = ActivityManager.getCurrentUser();
-        mAidCache.onWalletRoleHolderChanged(
-                mWalletRoleObserver.getDefaultWalletRoleHolder(currentUser), currentUser);
     }
 
     public INfcCardEmulation getNfcCardEmulationInterface() {
@@ -309,8 +307,10 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
     @Override
     public void onServicesUpdated(int userId, List<ApduServiceInfo> services,
             boolean validateInstalled) {
-        // Verify defaults are still the same
-        verifyDefaults(userId, services, validateInstalled);
+        if (!mWalletRoleObserver.isWalletRoleFeatureEnabled()) {
+            // Verify defaults are still the same
+            verifyDefaults(userId, services, validateInstalled);
+        }
         // Update the AID cache
         mAidCache.onServicesUpdated(userId, services);
         // Update the preferred services list

src/com/android/nfc/cardemulation/PreferredServices.java
@@ -134,8 +134,15 @@ public class PreferredServices implements com.android.nfc.ForegroundUtils.Callba
                 paymentForegroundUri,
                 true, mSettingsObserver, UserHandle.ALL);
 
+        int currentUserId = ActivityManager.getCurrentUser();
+
         // Load current settings defaults for payments
-        loadDefaultsFromSettings(ActivityManager.getCurrentUser(), false);
+        loadDefaultsFromSettings(currentUserId, false);
+
+        if (mWalletRoleObserver.isWalletRoleFeatureEnabled()) {
+            String holder = mWalletRoleObserver.getDefaultWalletRoleHolder(currentUserId);
+            onWalletRoleHolderChanged(holder, currentUserId);
+        }
     }
 
     private final class SettingsObserver extends ContentObserver {
@@ -253,7 +260,7 @@ public class PreferredServices implements com.android.nfc.ForegroundUtils.Callba
             }
         }
         // Notify if anything changed
-        if (paymentDefaultChanged || force) {
+        if (!mWalletRoleObserver.isWalletRoleFeatureEnabled() && (paymentDefaultChanged || force)) {
             mCallback.onPreferredPaymentServiceChanged(newUser.getIdentifier(), newDefault);
         }
         if (paymentPreferForegroundChanged || force) {

src/com/android/nfc/cardemulation/WalletRoleObserver.java
@@ -78,8 +78,6 @@ public class WalletRoleObserver {
 
     public void onUserSwitched(int userId) {
         String roleHolder = getDefaultWalletRoleHolder(userId);
-        if(!TextUtils.isEmpty(roleHolder)) {
-            mCallback.onWalletRoleHolderChanged(roleHolder, userId);
-        }
+        mCallback.onWalletRoleHolderChanged(roleHolder, userId);
     }
 }

commit 2cf7e8d46a109b9720fd7f96da23f9730f14b347

Correctly setup AID cache on boot
This was regressed in 3d7ca5072cde5e61eb09b42702544bcc7f4c224a

Bug: 329035965
Test: manual
Change-Id: I5a5cb1090e1127dc47e4243a5e144ab7ca316ee2

src/com/android/nfc/cardemulation/CardEmulationManager.java
@@ -145,6 +145,8 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
         mOffHostRouteUicc = mRoutingOptionManager.getOffHostRouteUicc();
         mForegroundUid = Process.INVALID_UID;
         int currentUser = ActivityManager.getCurrentUser();
+        onWalletRoleHolderChanged(
+                mWalletRoleObserver.getDefaultWalletRoleHolder(currentUser), currentUser);
     }
 
     public INfcCardEmulation getNfcCardEmulationInterface() {